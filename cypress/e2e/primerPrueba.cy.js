9501dc47756497c3fcd0fb6b9871aceb3e7fc2cb
Pacool84 <flopez_1384@hotmail.com>
2023-01-29 19:08:32 -0600

    [FIX] Structure of the tests

--- cypress/e2e/primerPrueba.cy.js
+++ cypress/e2e/primerPrueba.cy.js
@@ -1,11 +1,9 @@
-describe('Primer Prueba', () => { 
-  it("Navegar a mi primer pagina ", () =>{
-    cy.visit('https://www.platzi.com/')
-  })
- })
+describe("Primer Prueba", () => {
+  it("Navegar a mi primer pagina ", () => {
+    cy.visit("https://www.platzi.com/");
+  });
 
- describe("Segunda Prueba", () => {
-  it("Navegar por mi degunda prueba", () => {
-    cy.visit("https://www.google.com.mx")
-  })
- })
+  it("Navegar a mi segunda pagina", () => {
+    cy.visit("https://www.google.com");
+  });
+});
