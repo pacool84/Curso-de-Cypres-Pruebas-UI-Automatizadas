7de997b4f5262c5894149c626e82b9c95c2f2a82
Pacool84 <flopez_1384@hotmail.com>
2023-01-29 19:45:32 -0600

    [ADD] Navagation command to go forward

--- cypress/e2e/navegacion.cy.js
+++ cypress/e2e/navegacion.cy.js
@@ -12,7 +12,7 @@ describe("Navegacion", () => {
     cy.reload(true); /* Se hace que la recgarga no tenga CACHE */
   });
 
-  it.only("Navegar hacia una página previa", () => {
+  it("Navegar hacia una página previa", () => {
     cy.visit("https://www.google.com");
     cy.visit(
       "https://www.google.com/search?q=platzi&ei=zBzXY62gBYmBxc8P8YqdoAQ&ved=0ahUKEwittqTZke78AhWJQPEDHXFFB0QQ4dUDCBA&uact=5&oq=platzi&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzINCC4QsQMQxwEQ0QMQQzIICAAQgAQQsQMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIYCC4QsQMQxwEQ0QMQQxDcBBDeBBDgBBgDOgoIABBHENYEELADOgcIABCwAxBDOg0IABDkAhDWBBCwAxgBOgwILhDIAxCwAxBDGAI6CwgAEIAEELEDEIMBOgsILhCABBCxAxCDAToICAAQsQMQgwE6CggAELEDEIMBEEM6BAgAEEM6BwgAELEDEEM6BwguENQCEEM6CggAEIAEELEDEAo6BwgAEIAEEApKBAhBGABKBAhGGAFQrwpYhRJgqR9oAnABeACAAdYFiAG0IJIBBTUtNC4ymAEAoAEByAETwAEB2gEGCAEQARgJ2gEGCAIQARgI2gEGCAMQARgU&sclient=gws-wiz-serp"
@@ -20,5 +20,14 @@ describe("Navegacion", () => {
     cy.go(
       "back"
     ); /* Se puede obtener el mismo resultado realizando cy.go(-1) */
+  });
+
+  it.only("Avanzar hacia una pagina", () => {
+    cy.visit("https://www.google.com");
+    cy.visit(
+      "https://www.google.com/search?q=platzi&ei=zBzXY62gBYmBxc8P8YqdoAQ&ved=0ahUKEwittqTZke78AhWJQPEDHXFFB0QQ4dUDCBA&uact=5&oq=platzi&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzINCC4QsQMQxwEQ0QMQQzIICAAQgAQQsQMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIYCC4QsQMQxwEQ0QMQQxDcBBDeBBDgBBgDOgoIABBHENYEELADOgcIABCwAxBDOg0IABDkAhDWBBCwAxgBOgwILhDIAxCwAxBDGAI6CwgAEIAEELEDEIMBOgsILhCABBCxAxCDAToICAAQsQMQgwE6CggAELEDEIMBEEM6BAgAEEM6BwgAELEDEEM6BwguENQCEEM6CggAEIAEELEDEAo6BwgAEIAEEApKBAhBGABKBAhGGAFQrwpYhRJgqR9oAnABeACAAdYFiAG0IJIBBTUtNC4ymAEAoAEByAETwAEB2gEGCAEQARgJ2gEGCAIQARgI2gEGCAMQARgU&sclient=gws-wiz-serp"
+    );
+    cy.go("back");
+    cy.go(1);
   });
 });
