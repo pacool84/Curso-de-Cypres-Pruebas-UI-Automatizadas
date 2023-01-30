bc2fb80df6341bb52b5cf6492706d865037e1d0d
Pacool84 <flopez_1384@hotmail.com>
2023-01-29 19:47:25 -0600

    [CHANGE] The index instruction to go forward

--- cypress/e2e/navegacion.cy.js
+++ cypress/e2e/navegacion.cy.js
@@ -28,6 +28,8 @@ describe("Navegacion", () => {
       "https://www.google.com/search?q=platzi&ei=zBzXY62gBYmBxc8P8YqdoAQ&ved=0ahUKEwittqTZke78AhWJQPEDHXFFB0QQ4dUDCBA&uact=5&oq=platzi&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzINCC4QsQMQxwEQ0QMQQzIICAAQgAQQsQMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIYCC4QsQMQxwEQ0QMQQxDcBBDeBBDgBBgDOgoIABBHENYEELADOgcIABCwAxBDOg0IABDkAhDWBBCwAxgBOgwILhDIAxCwAxBDGAI6CwgAEIAEELEDEIMBOgsILhCABBCxAxCDAToICAAQsQMQgwE6CggAELEDEIMBEEM6BAgAEEM6BwgAELEDEEM6BwguENQCEEM6CggAEIAEELEDEAo6BwgAEIAEEApKBAhBGABKBAhGGAFQrwpYhRJgqR9oAnABeACAAdYFiAG0IJIBBTUtNC4ymAEAoAEByAETwAEB2gEGCAEQARgJ2gEGCAIQARgI2gEGCAMQARgU&sclient=gws-wiz-serp"
     );
     cy.go("back");
-    cy.go(1);
+    cy.go(
+      "forward"
+    ); /* Se puede obtener el mismo resultado realizando cy.go(1) */
   });
 });
