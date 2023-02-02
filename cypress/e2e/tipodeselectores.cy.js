20595f864d9900a04d3b0d826ebfbbbee91b7426
Pacool84 <flopez_1384@hotmail.com>
2023-02-01 21:54:50 -0600

    [ADD] More methods to find tags, class or attributes - contains() and find()

--- cypress/e2e/tipodeselectores.cy.js
+++ cypress/e2e/tipodeselectores.cy.js
@@ -1,5 +1,5 @@
 describe("Probando configuracion", () => {
-  it.only("Obteniendo por un tag", () => {
+  it("Obteniendo por un tag", () => {
     cy.visit("https://cuevana3.it/");
     cy.get("input");
   });
@@ -22,8 +22,23 @@ describe("Probando configuracion", () => {
     ); /*  Considerar que un elemento puede tener varias clases, en ese caso tenemos que unirlas por medio de un . punto */
   });
 
-  it.only("Obteniendo por un tag", () => {
+  it("Obteniendo por un tag", () => {
+    cy.get("#keysssb").type(
+      "Top Gun and all the fucking movies that I want F U C K Y E A H B I T C H E S"
+    );
+  });
+
+  it("Usando el CONTAINS", () => {
+    cy.contains("ingresar a cuevana");
+    cy.contains(
+      ".mankeleta",
+      "ingresa a cuevana"
+    ); /* Seleccionando por texto que contenga en una clase y por puro texto */
+  });
+
+  it.only("Identificando el PARENT", () => {
     cy.visit("https://cuevana3.it/");
-    cy.get("#keysssb").type("Top Gun and all the fucking movies that I want");
+    cy.get("#keysssb").parent();
+    cy.get(".main-site").parents().find("label");
   });
 });
